945b1ced275a4034e1bf5ca963df8c4dc821f31e
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-07-13 09:13:29 +0530

    update header and css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import CMSPage from './pages/CMSPage';
+import './App.css';
 
 function App() {
   return (
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
+import logo from "../../assets/dropdown icon.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">ABC Company</div>
-      <nav className="nav-links">
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
+      <div className="header-inner">
+        <nav className="nav-links">
+          <a href="#about">About</a>
+          <div className="services-dropdown">
+            <a href="#services">Services</a>
+            <img src={logo} alt="dropdown" width={12} />
+          </div>
+        </nav>
+
         <a href="/cms">
           <button className="admin-btn">Admin</button>
         </a>
-      </nav>
+      </div>
     </header>
   );
 };
